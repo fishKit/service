844878bbc9694f3c5f4694efdff276989be9af45
huangzhijie <huangzhijie@gdcattsoft.com>
2020-07-16 20:53:34 +0800

    service1集成swagger

--- src/main/java/com/catt/service1/test/controller/TestController.java
+++ src/main/java/com/catt/service1/test/controller/TestController.java
@@ -5,6 +5,9 @@ import com.catt.service1.test.entity.TCutoverRelAlarm;
 import com.catt.service1.test.mapper.CommonMapper;
 import com.catt.service1.test.mapper.TestMapper;
 import com.catt.service1.test.service.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,7 @@ import java.util.Map;
  * @time: 2020/5/22 18:15
  */
 @RestController
+@Api(value = "端口测试啊")
 @RequestMapping("/test")
 public class TestController {
     @Value("${server.port}")
@@ -33,16 +37,20 @@ public class TestController {
     @Autowired
     private CommonMapper commonMapper;
 
+    @ApiOperation(value = "获取当前端口号", notes = "测试获取端口")
     @GetMapping(value = "/getTestPort")
     public String getTestPort() {
         return "当前的端口号是：" + port;
     }
 
+    @ApiOperation(value = "获取当前输入得端口号", notes = "测试获取输入端口")
+    @ApiImplicitParam(paramType = "query", name = "id", value = "端口id", required = true, dataType = "String")
     @GetMapping(value = "/getTestPort1")
     public String getTestPort(@RequestParam(value = "id", required = false) String id) {
         return "当前的端口号是：" + id;
     }
 
+    @ApiOperation(value = "获取当前告警", notes = "测试获取告警")
     @GetMapping(value = "/list/alarm")
     public List<TCutoverRelAlarm> alarm() {
         return testMapper.selectList(null);
